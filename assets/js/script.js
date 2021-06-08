c1e146950fd78963e3d0f9bb322a34f225712803
Cole Sammons <colemsammons@gmail.com>
2021-06-07 21:57:46 -0700

    console logged omdb/utelly responses

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// 7af753d8       //API key!!!!
+var apikey = "7af753d8";
+var url = "http://www.omdbapi.com/?apikey=" + apikey;
+var movie = "Jaws";
+//omdb api call
+$.ajax({
+    method:'GET',
+    url:url + "&t=" + movie,
+    success:function(data) {
+        console.log(data);
+    }
+})
+//utelly settings to pass url
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?source_id=tt0073195&source=imdb&country=us",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "69b980341fmsh2b7d3a3dc6d32b3p1f8da7jsn0c865af8dcf9",
+		"x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com"
+	}
+};
+//utelly api call
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
