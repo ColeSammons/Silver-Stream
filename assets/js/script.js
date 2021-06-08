ab24896d02a685bb18103b86fd4fc90f644704d4
Cole Sammons <colemsammons@gmail.com>
2021-06-08 16:23:56 -0700

    created getRating. Returns rating of id given

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var urlTopRated = "https://api.themoviedb.org/3/movie/top_rated?api_key=" + apiK
 var urlPoster = "https://image.tmdb.org/t/p/original";//url for posters. Image location should be added on.
 var genres = []
 
-var movieID = "tt0088247"//hardcode for Jaws from imbd to test fetch calls   tt0073195
+var movieID = "";//Initializing variable that will be passed into getRating().Use "tt0073195" to test getRating with imdb id for Jaws
 
 //asynchronous function to log top rated movies. Can access poster and display on screen. Can display other pages by adding on &page=# to the url
 $.ajax({
@@ -23,20 +23,22 @@ $.ajax({
 })
 
 //utelly settings to pass url
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?source_id=" + movieID + "&source=imdb&country=us",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "69b980341fmsh2b7d3a3dc6d32b3p1f8da7jsn0c865af8dcf9",
-		"x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com"
-	}
-};
-//utelly api call
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+var utelly = function(movieID) {
+	const settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?source_id=" + movieID + "&source=imdb&country=us",
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-key": "69b980341fmsh2b7d3a3dc6d32b3p1f8da7jsn0c865af8dcf9",
+			"x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com"
+		}
+	};
+	//utelly api call
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+	});
+}
 
 //Filling the genres array with genre objects that have a name and id available
 var getGenres = function () {
@@ -57,6 +59,25 @@ var getGenres = function () {
 	console.log(genres);
 }
 
+//Function to log movie rating
+var getRating = function (movieID) {
+	var urlMovieRating = "https://api.themoviedb.org/3/movie/" + movieID + "/release_dates?api_key=" + apiKeyTMDB + "&language=en";
+	$.ajax({
+		method: 'GET',
+		url: urlMovieRating,
+		success: function (data) {
+			console.log(data.results);
+			for (var i = 0; i < data.results.length; i++) {
+				if(data.results[i].iso_3166_1 === "US") {
+					var rating = data.results[i].release_dates[data.results[i].release_dates.length - 1].certification
+					console.log(rating);
+				}
+			}
+			return rating;
+		}
+	})
+}
 
-
-getGenres();
+var test1 = getRating("tt0073195");//Getting the rating for Jaws using imdb ID
+utelly("tt0073195");//Getting streaming info for Jaws using imdb ID
+getGenres();//Filling genres array
