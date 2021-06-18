a7fdfb6cffa95f0993a62ddf3e4e842c5d3d9991
Cole Sammons <colemsammons@gmail.com>
2021-06-18 09:37:32 -0700

    connected html documents

---
+++ assets/js/homepage.js
@@ -0,0 +1,85 @@
+$(document).foundation();//initializes foundation
+var apiKeyTMDB = "8b0814d7463c28b76f719e9285aecbd7";
+var urlGenre = "https://api.themoviedb.org/3/genre/movie/list?api_key=" + apiKeyTMDB + "&language=en";//links to an array of TMDB genres categorized by id
+var urlTopRated = "https://api.themoviedb.org/3/movie/top_rated?api_key=" + apiKeyTMDB + "&language=en";//links to top rated TMDB movies
+var movies = [];
+var genres = [];
+
+//Filling the genres array with genre objects that have a name and id available
+var getGenreArray = function () {
+	fetch(urlGenre).then(function (response) {
+		return response.json();
+	}).then(function (data) {
+		for (var i = 0; i < data.genres.length; i++) {
+			var genreData = {
+				name: data.genres[i].name,
+				id: data.genres[i].id
+			};
+			genres.push(genreData);
+		}
+	})
+	console.log("Array of genres");
+	console.log(genres);
+}
+
+//Gets an array of top rated movies
+var topRated = function () {
+	for (let i = 0; i < 55; i++) {
+		var urlTopRated = "https://api.themoviedb.org/3/movie/top_rated?api_key=" + apiKeyTMDB + "&language=en&page=" + Math.random() * 200;//randomizing pages to display
+		fetch(urlTopRated).then(function (response) {
+			if (response.ok) {
+				response.json()
+                .then(async function (data) {
+					for (let j = 0; j < data.results.length; j++) {
+						if (data.results[j].original_language === "en") {
+							var movie = {
+								'genre': data.results[j].genre_ids,
+								'id': data.results[j].id,
+								'overview': data.results[j].overview,
+								'poster': data.results[j].poster_path,
+								'release': data.results[j].release_date,
+								'title': data.results[j].title
+							}
+							movies.push(movie)
+						}
+					}
+				})
+			}
+
+		})
+	}
+}
+
+//Gets imdb ID
+var getExternalID = function () {
+	setTimeout(function () {
+		for (let i = 0; i < movies.length; i++) {
+			// console.log(movies[i]);
+			var externalIDURL = "https://api.themoviedb.org/3/movie/" + movies[i].id + "?api_key=" + apiKeyTMDB;
+			fetch(externalIDURL).then(async function (response) {
+				if (response.ok) {
+					return response.json()
+						.then((data) => {
+							movies[i].imdb = data.imdb_id;
+						})
+				}
+			})
+		}
+	}, 500);
+}
+
+
+var saveLocal = function() {
+    setTimeout(function() {
+        window.localStorage.removeItem("movies");
+        console.log("hello");
+        window.localStorage.setItem("movies", JSON.stringify(movies));
+    },500)
+}
+
+
+
+getGenreArray();
+topRated();
+getExternalID();
+saveLocal();
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,12 @@
 $(document).foundation();//initializes foundation
-var apiKeyTMDB = "8b0814d7463c28b76f719e9285aecbd7";
-var urlGenre = "https://api.themoviedb.org/3/genre/movie/list?api_key=" + apiKeyTMDB + "&language=en";//links to an array of TMDB genres categorized by id
-var urlTopRated = "https://api.themoviedb.org/3/movie/top_rated?api_key=" + apiKeyTMDB + "&language=en";//links to top rated TMDB movies
 var urlPoster = "https://image.tmdb.org/t/p/original";//url for posters. Image location should be added on.
 var sortedGenre = [];
-var movies = [];
-var genres = [];
-
-var topRated = function () {
-	for (let i = 0; i < 55; i++) {
-		var urlTopRated = "https://api.themoviedb.org/3/movie/top_rated?api_key=" + apiKeyTMDB + "&language=en&page=" + Math.random() * 200;//randomizing pages to display
-		fetch(urlTopRated).then(function (response) {
-			if (response.ok) {
-				response.json().then(async function (data) {
-					for (let j = 0; j < data.results.length; j++) {
-						if (data.results[j].original_language === "en") {
-							var movie = {
-								'genre': data.results[j].genre_ids,
-								'id': data.results[j].id,
-								'overview': data.results[j].overview,
-								'poster': data.results[j].poster_path,
-								'release': data.results[j].release_date,
-								'title': data.results[j].title
-							}
-							movies.push(movie)
-						}
-					}
-				})
-			}
-
-		})
-	}
-}
+movies = [];
 
 var sortGenre = function (event) {
-	var genreID = event.target.id;
+	var url = document.location.href;
+	var genreID = url.split("=")[1];
+
 	sortedGenre = [];
 	$(".accordion-item").remove();
 	console.log(genreID);
@@ -84,40 +56,6 @@ var utelly = function (event) {
 		});
 }
 
-//Filling the genres array with genre objects that have a name and id available
-var getGenreArray = function () {
-	fetch(urlGenre).then(function (response) {
-		return response.json();
-	}).then(function (data) {
-		for (var i = 0; i < data.genres.length; i++) {
-			var genreData = {
-				name: data.genres[i].name,
-				id: data.genres[i].id
-			};
-			genres.push(genreData);
-		}
-	})
-	console.log("Array of genres");
-	console.log(genres);
-}
-
-var getExternalID = function () {
-	setTimeout(function () {
-		for (let i = 0; i < movies.length; i++) {
-			// console.log(movies[i]);
-			var externalIDURL = "https://api.themoviedb.org/3/movie/" + movies[i].id + "?api_key=" + apiKeyTMDB;
-			fetch(externalIDURL).then(async function (response) {
-				if (response.ok) {
-					return response.json()
-						.then((data) => {
-							movies[i].imdb = data.imdb_id;
-						})
-				}
-			})
-		}
-	}, 500);
-}
-
 var displaySorted = function () {
 	for (var i = 0; i < sortedGenre.length; i++) {
 		//accordian for movies.
@@ -141,29 +79,33 @@ var displaySorted = function () {
 	}
 }
 
-$("#28").on("click", sortGenre);
-$("#12").on("click", sortGenre);
-$("#16").on("click", sortGenre);
-$("#35").on("click", sortGenre);
-$("#80").on("click", sortGenre);
-$("#99").on("click", sortGenre);
-$("#18").on("click", sortGenre);
-$("#10751").on("click", sortGenre);
-$("#14").on("click", sortGenre);
-$("#36").on("click", sortGenre);
-$("#27").on("click", sortGenre);
-$("#10402").on("click", sortGenre);
-$("#9648").on("click", sortGenre);
-$("#10749").on("click", sortGenre);
-$("#878").on("click", sortGenre);
-$("#10770").on("click", sortGenre);
-$("#53").on("click", sortGenre);
-$("#10752").on("click", sortGenre);
-$("#37").on("click", sortGenre);
+var loadPage = function() {
+	movies = JSON.parse(localStorage.getItem("movies"));
+	console.log(movies);
+	sortGenre()
+}
+
+// $("#28").on("click", sortGenre);
+// $("#12").on("click", sortGenre);
+// $("#16").on("click", sortGenre);
+// $("#35").on("click", sortGenre);
+// $("#80").on("click", sortGenre);
+// $("#99").on("click", sortGenre);
+// $("#18").on("click", sortGenre);
+// $("#10751").on("click", sortGenre);
+// $("#14").on("click", sortGenre);
+// $("#36").on("click", sortGenre);
+// $("#27").on("click", sortGenre);
+// $("#10402").on("click", sortGenre);
+// $("#9648").on("click", sortGenre);
+// $("#10749").on("click", sortGenre);
+// $("#878").on("click", sortGenre);
+// $("#10770").on("click", sortGenre);
+// $("#53").on("click", sortGenre);
+// $("#10752").on("click", sortGenre);
+// $("#37").on("click", sortGenre);
 
 
 
 
-getGenreArray();
-topRated();
-getExternalID();
+loadPage();
